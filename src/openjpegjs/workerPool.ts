e8cdac158c55d31312cf3fbb8a6383e4d78b2bf7
peter_todd <ptr.tdd@gmail.com>
2021-02-05 18:45:28 +0000

    add some logging & option to kill worker on release.

--- src/openjpegjs/workerPool.ts
+++ src/openjpegjs/workerPool.ts
@@ -24,20 +24,22 @@ export class WorkerPool {
         });
         return promise;
     }
-    releaseWorker(worker: Worker) {
-        worker = this.maybeRetireWorker(worker);
+    releaseWorker(worker: Worker, kill = false) {
+        const nextWorker = kill ? this.terminateWorker(worker) : this.maybeRetireWorker(worker);
         if (this.backlog.length > 0) {
-            this.backlog.shift()!(worker);
+            this.backlog.shift()!(nextWorker);
         } else {
-            this.idle.push(worker);
+            this.idle.push(nextWorker);
         }
     }
     private newWorker() {
         const w = new Worker(this.scriptName);
         this.workerAge.set(w, 0);
+        console.log(`newWorker() : current count: ${this.workerAge.size}`);
         return w;
     }
     //I seem to face ever-growing heap, so simplest strategy appears to be to terminate
+    //(or, y'know, not leak memory)
     private maybeRetireWorker(worker: Worker) {
         const age = this.workerAge.get(worker)! + 1;
         if (age > this.maxAge) {
@@ -46,12 +48,17 @@ export class WorkerPool {
         this.workerAge.set(worker, age);
         return worker;
     }
-    /**might want to call this from outside if we have reason to believe internal state is compromised.
-     * -- if doing so *DON'T* use the returned newWorker --
-    */
-    terminateWorker(worker: Worker) {
+    private terminateWorker(worker: Worker) {
         worker.terminate();
-        this.workerAge.delete(worker);
+        if (!this.workerAge.has(worker)) {
+            throw new Error("tried to delete worker that isn't in workerAge");
+        }
+        if (!this.workerAge.delete(worker)) {
+            console.warn(`workerAge.delete() returned false!`);
+        } else {
+            console.log(`workerAge.delete() ok`);
+        }
+        console.log(`terminateWorker() : current count: ${this.workerAge.size}`);
         return this.newWorker();
     }
 }
