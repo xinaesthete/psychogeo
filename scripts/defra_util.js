93bb9e22941d114c791d87ee41affd9aed66e165
Peter Todd <ptr.tdd@gmail.com>
2021-06-14 14:17:03 +0100

    add basic conversion from img coords to OS. Save metadata.

--- scripts/defra_util.js
+++ scripts/defra_util.js
@@ -4,9 +4,15 @@ const fs = require('fs');
 const readline = require('readline');
 
 const path = '/Volumes/BlackSea/GIS/DEFRA/LIDAR_10m_DTM_Composite_2019/LIDAR_10m_DTM_Composite.tif';
-const outPath = '/Volumes/BlackSea/GIS/DEFRA/LIDAR_10m_DTM_Composite_2019/';
+const outPath = '/Volumes/BlackSea/GIS/DEFRA/LIDAR_10m_DTM_Composite_2019/htj2k/';
 let encoder; //hacky async init this later when 'probably' done.
 //const encoder = new jph.HTJ2KEncoder();
+let decoder;
+const compressedFiles = fs.readdirSync(outPath).filter(f=>
+  !f.startsWith('.') && f.endsWith('j2c') && f.includes('32bit'));
+const BAD_VALUE = 3.402823466e38; //not in a form useful for direct comparison.
+const BAD_THRESH = 3e8;
+const NEW_BAD_VALUE = -200;
 
 //https://discourse.threejs.org/t/three-datatexture-works-on-mobile-only-when-i-keep-the-type-three-floattype-but-not-as-three-halffloattype/1864/4
 const floatView = new Float32Array(1);
@@ -48,14 +54,15 @@ function toHalf(val) {
     bits += m & 1;
     return bits;
 }
-async function processWindow(data, frameInfo, name) {
-  //TODO: encode to HTJ2K or whatever we decide we want here
-  const [vals] = data;
-  const anyGood = vals.find(v => v < 3000);
+
+async function compressAndSave(data, frameInfo, name) {
+  let [vals] = data;
+  const anyGood = vals.find(v => v < BAD_THRESH);
   if (anyGood === undefined) {
     // console.log('no useful data...');
     return false;
   }
+  vals = vals.map(v => v > BAD_THRESH ? NEW_BAD_VALUE : v);
   //encode the data to a file... keep it at 32bit float for now?
   const uncompressedBuffer = encoder.getDecodedBuffer(frameInfo);
   uncompressedBuffer.set(vals);
@@ -64,55 +71,132 @@ async function processWindow(data, frameInfo, name) {
   console.log(`recompressed size ${encoded.byteLength / (1024)}kb`);
   fs.writeFile(outPath + name, new Uint8Array(encoded), ()=>console.log(`wrote "${name}"`));
 
-  // const min = vals.reduce((a,b) => Math.min(a, b), Number.MAX_VALUE);
-  // const max = vals.reduce((a,b) => Math.max(a, b), Number.MIN_VALUE);
-  // const mean = vals.reduce((a, b) => a+b, 0) / vals.length;
-  
-  // console.log('got stats in', Date.now()-t);
-  // console.log('min', min);
-  // console.log('max', max);
-  // console.log('mean', mean);
   return true;
+}
+function printStats(vals) {
+  const t = Date.now();
+  const min = vals.reduce((a,b) => Math.min(a, b), Number.MAX_VALUE);
+  const max = vals.reduce((a,b) => Math.max(a, b), Number.MIN_VALUE);
+  const mean = vals.reduce((a, b) => a+b, 0) / vals.length;
+  
+  console.log('got stats in', Date.now()-t);
+  console.log('min', min);
+  console.log('max', max);
+  console.log('mean', mean);
+}
+function initHTJ2K() {
+  encoder = new jph.HTJ2KEncoder(); //probably ready now...
+  decoder = new jph.HTJ2KDecoder();
+  encoder.setQuality(true, 0);
+  //encoder.setCompressionRatio(0, 0); //not a function??
+  encoder.setDecompositions(8);
 }
 //const pool = new GeoTIFF.Pool(); //seems to take ~2x as long to readRasters with pool.
 GeoTIFF.fromFile(path).then(async (tiff) => {
   const image = await tiff.getImage();
-  encoder = new jph.HTJ2KEncoder(); //probably ready now...
-  encoder.setQuality(1, 1);
-  //encoder.setCompressionRatio(0, 0); //not a function??
-  encoder.setDecompositions(8);
-
   console.log('file opened!', image.getWidth(), image.getHeight(), image.getTileWidth(), image.getTileHeight());
+  
+  initHTJ2K();
+
   const w = image.getWidth();
   const h = image.getHeight();
+  const [L, T, R, B] = box = image.getBoundingBox();
+  //*10 is because these are 10m pixels. no fancy projection required for now.
+  if ((R - L) !== w*10) console.error(`!!! width of bounding box (${R-L}) not equal to 10*width of image (${w*10}) !!!`);
+  if ((B - T) !== h*10) console.error(`!!! height of bounding box (${B-T}) not equal to 10*height of image (${h*10}) !!!`);
   console.log('samples per pixel:', image.getSamplesPerPixel());
-  const s = outTileSize = image.getTileWidth() * 16; //tempted to make this POT, have some overlap on each tile...
+  const s = outTileSize = image.getTileWidth() * 16; //somewhat magic numbers here
+  console.log('tile size: ', s);
   const nx = Math.floor(w/outTileSize), ny = Math.floor(h/outTileSize);
-  console.log(`making ${nx}x${ny} tiles...`);
-  const frameInfo = { bitsPerSample: 32, isSigned: true, width: s, height: s, componentCount: 1 };
-  for (let y=0; y<ny; y++) {
-    console.log(`row ${y+1}/${ny}...`);
-    //readline.cursorTo(process.stdout, 0, 0);
-    //process.stdout.write(x + '/' + nx);
-    // readline.cursorTo(process.stdout, 0, y);
-    // process.stdout.write('\r' + " ".repeat(1000));
-    for (let x=0; x<nx; x++) {
-      // readline.cursorTo(process.stdout, x, y);
-      // console.log(`col ${y+1}/${ny}...`);
-      //let's not commit to the full processing until we do a trial...      
-      const left = x*s, right = (x+1)*s, top = y*s, bottom = (y+1)*s;
-      const win = [left, top, right, bottom];
-      // console.log(...win);
-      //image window can go beyond bounds, in which case it might be useful to supply a fillValue
-      const t = Date.now();
-      const data = await image.readRasters({window: win, fillValue: -1});
-      // console.log(`got data in`, Date.now()-t);
-      const X = x.toLocaleString(undefined, {minimumIntegerDigits: 3});
-      const Y = y.toLocaleString(undefined, {minimumIntegerDigits: 3});
-      const r = await processWindow(data, frameInfo, `${X}_${Y}-32bit.j2c`);
-      // process.stdout.write(r ? '.' : ' ');
-    }
+  
+  function geoLLCoordFromWindowOrName(win) {
+    if (typeof win === 'string') win = windowFromImgName(win);
+    const [l, t, r, b] = win.map(v=>v*10);
+    const xll = l+L;
+    const yll = B-b; //b-T;
+    return [xll, yll];
   }
+  /** assuming tile size 4096, images named 'xxx_yyy-32bit.j2c' where xxx & yyy are indices based on that tile size*/
+  function windowFromImgName(f) {
+    const x = Number.parseInt(f.substring(0, 3));
+    const y = Number.parseInt(f.substring(4, 7));
+    const left = x*s, right = (x+1)*s, top = y*s, bottom = (y+1)*s;
+    return [left, top, right, bottom];
+  }
+
+  (async function compare() {
+    for (let i=0; i<compressedFiles.length; i++) {
+      const f = compressedFiles[i];
+      const win = windowFromImgName(f);
+      const encodedBitstreamP = fs.promises.readFile(outPath + f);
+      const referenceDataP = image.readRasters({window: win, fillValue: NEW_BAD_VALUE});
+      const [encodedBitstream, referenceDataA] = await Promise.all([encodedBitstreamP, referenceDataP]);
+      let [referenceData] = referenceDataA;
+      referenceData = referenceData.map(v => v > BAD_THRESH ? NEW_BAD_VALUE : v);
+      const encodedBuffer = decoder.getEncodedBuffer(encodedBitstream.length);
+      encodedBuffer.set(encodedBitstream);
+      decoder.decode();
+      const decRaw = decoder.getDecodedBuffer(); //Uint8Array
+      const decodedBuffer = new Float32Array(decRaw, decRaw.byteOffset, decRaw.byteLength/4);
+      console.log('reference data', f);
+      printStats(referenceData);
+      const info = decoder.getFrameInfo();
+      console.log('decoded data', JSON.stringify(info));
+      printStats(decodedBuffer);
+
+      const a = referenceData[0], b = decodedBuffer[0];
+      console.log(a, b, Math.sqrt((a-b)**2));
+      const errSq = (v, i) => {
+        const ref = referenceData[i];
+        const r = ref > BAD_THRESH ? NEW_BAD_VALUE : ref;
+        return (v-r)**2;
+      }
+      const mse = decodedBuffer.map(errSq).reduce((a,b)=>a+b, 0) / decodedBuffer.length;
+      const rmse = Math.sqrt(mse);
+      console.log('rmse:', rmse);
+    }
+  })();
+  (async function compress() {
+    console.log(`making ${nx}x${ny} tiles...`);
+    //https://github.com/chafey/openjphjs/blob/master/src/FrameInfo.hpp
+    //bitsPerSample range [2, 16]?
+    const frameInfo = { bitsPerSample: 32, isSigned: true, width: s, height: s, componentCount: 1 };
+    for (let y=0; y<ny; y++) {
+      console.log(`row ${y+1}/${ny}...`);
+      //readline.cursorTo(process.stdout, 0, 0);
+      //process.stdout.write(x + '/' + nx);
+      // readline.cursorTo(process.stdout, 0, y);
+      // process.stdout.write('\r' + " ".repeat(1000));
+      for (let x=0; x<nx; x++) {
+        // readline.cursorTo(process.stdout, x, y);
+        // console.log(`col ${y+1}/${ny}...`);
+        const left = x*s, right = (x+1)*s, top = y*s, bottom = (y+1)*s;
+        const win = [left, top, right, bottom];
+        // console.log(...win);
+        //image window can go beyond bounds, in which case it might be useful to supply a fillValue
+        const t = Date.now();
+        const X = x.toLocaleString(undefined, {minimumIntegerDigits: 3});
+        const Y = y.toLocaleString(undefined, {minimumIntegerDigits: 3});
+        const name = `${X}_${Y}-32bit.j2c`;
+        // if (compressedFiles.includes(name)) continue;
+        const data = await image.readRasters({window: win, fillValue: NEW_BAD_VALUE});
+        // console.log(`got data in`, Date.now()-t);
+        const r = await compressAndSave(data, frameInfo, name);
+        // process.stdout.write(r ? '.' : ' ');
+      }
+    }
+  });
+  (async function createAllMetadata() {
+    const catalog = {};
+    compressedFiles.forEach(async f => {
+      const [xll, yll] = geoLLCoordFromWindowOrName(f);
+      const entry = catalog[`${xll}, ${yll}`] = {xllcorner: xll, yllcorner: yll, nrows: s, ncols: s, source_filename: f};
+      const json = JSON.stringify(entry);
+      fs.writeFile(outPath+f+'.json', json, ()=>{console.log('wrote metadata for', f)});
+    });
+
+    fs.writeFile(outPath+'10m_dtm_catalog.json', JSON.stringify(catalog, null, 2), ()=>{console.log('wrote catalog')});
+  });
 }).catch(err => {
   console.error('ERROR!');
   console.error(err);
