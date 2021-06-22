d5703c6b3c5b073974ef7ce55e36bdb609d1f4da
Peter Todd <ptr.tdd@gmail.com>
2021-06-22 12:17:16 +0100

    data from tiff converted to HalfFloat texture.

--- src/openjpegjs/jp2kloader.ts
+++ src/openjpegjs/jp2kloader.ts
@@ -22,11 +22,11 @@ export interface TextureTile {
 }
 export interface PixFrame {
   frameInfo: FrameInfo;
-  pixData: Uint16Array | Float32Array;
+  pixData: Uint16Array;
 }
 export interface TexFrame {
   frameInfo: FrameInfo;
-  texData: Uint16Array | Float32Array;
+  texData: Uint16Array;
   //not all code here is very clean wrt naming etc. at the moment.
 }
 
@@ -43,7 +43,8 @@ async function getTexData(url: string, fullFloat: boolean, compressionRatio = 1)
     // for some reason, this doesn't appear to resemble the data that I'm expecting.
     // I get an error that the byteLength is not a multiple of 4.
     // If I try to slice a portion from the start & turn that into a Float32Array, it doesn't appear to contain relevant values.
-    const texData = new Float32Array(buf);//.map(v => Number.isNaN(v) ? -200 : v);
+    const texData = new Uint16Array(new Float32Array(buf).map(toHalf));//.map(v => Number.isNaN(v) ? -200 : v);
+    // const texData = new Float32Array(buf);
     if (texData.length !== frameInfo.width * frameInfo.height) {
       console.error(`that ain't gonna work - ${texData.length} isn't expected length (${frameInfo.width*frameInfo.height})`);
     }
@@ -77,10 +78,10 @@ async function getTexData(url: string, fullFloat: boolean, compressionRatio = 1)
 }
 const textureCache = new Map<string, TextureTile>();
 
-export async function jp2Texture(url: string, fullFloat: boolean) {
+export async function jp2Texture(url: string, simplerDecodeHack: boolean) {
   //what if someone else is already waiting for it but it's not in the cache yet?
   if (textureCache.has(url)) return textureCache.get(url) as TextureTile;
-  const result = await getTexData(url, fullFloat);
+  const result = await getTexData(url, simplerDecodeHack);
   const frameInfo = result.frameInfo;
   // console.log(JSON.stringify(frameInfo, null, 2));
 
@@ -92,11 +93,12 @@ export async function jp2Texture(url: string, fullFloat: boolean) {
   //internalFormat = gl.DEPTH_COMPONENT16; format = gl.DEPTH_COMPONENT; type = gl.UNSIGNED_SHORT; // OK, red
   // const texture = new THREE.DataTexture(result.texData, frameInfo.width, frameInfo.height, THREE.RGBFormat, THREE.UnsignedByteType);
   const format = THREE.RedFormat;
-  const type = fullFloat ? THREE.FloatType : THREE.HalfFloatType;
+  const type = THREE.HalfFloatType;
   const d = result.texData; //fullFloat ? new Float32Array(result.texData) : result.texData;
   const texture = new THREE.DataTexture(d, frameInfo.width, frameInfo.height, format, type);
   texture.minFilter = texture.magFilter = THREE.LinearFilter;
   texture.wrapS = texture.wrapT = THREE.ClampToEdgeWrapping;
+  texture.anisotropy = 16;
   texture.generateMipmaps = true; //TODO: test & make sure full use being made...
   const t = {texture, frameInfo};
   textureCache.set(url, t);
@@ -106,4 +108,49 @@ export async function jp2Texture(url: string, fullFloat: boolean) {
 export function newGLContext() {
   //TODO: formalise GL resource management with threact.
   textureCache.clear();
+}
+
+
+//https://discourse.threejs.org/t/three-datatexture-works-on-mobile-only-when-i-keep-the-type-three-floattype-but-not-as-three-halffloattype/1864/4
+const floatView = new Float32Array(1);
+const int32View = new Int32Array(floatView.buffer);
+function toHalf(val: number) {
+    floatView[0] = val;
+    var x = int32View[0];
+
+    var bits = (x >> 16) & 0x8000; /* Get the sign */
+    var m = (x >> 12) & 0x07ff; /* Keep one extra bit for rounding */
+    var e = (x >> 23) & 0xff; /* Using int is faster here */
+
+    /* If zero, or denormal, or exponent underflows too much for a denormal
+                   * half, return signed zero. */
+    if (e < 103) return bits;
+
+    /* If NaN, return NaN. If Inf or exponent overflow, return Inf. */
+    if (e > 142) {
+
+        bits |= 0x7c00;
+        /* If exponent was 0xff and one mantissa bit was set, it means NaN,
+                                 * not Inf, so make sure we set one mantissa bit too. */
+        bits |= ((e == 255) ? 0 : 1) && (x & 0x007fffff);
+        return bits;
+
+    }
+
+    /* If exponent underflows but not too much, return a denormal */
+    if (e < 113) {
+
+        m |= 0x0800;
+        /* Extra rounding may overflow and set mantissa to 0 and exponent
+                         * to 1, which is OK. */
+        bits |= (m >> (114 - e)) + ((m >> (113 - e)) & 1);
+        return bits;
+
+    }
+
+    bits |= ((e - 112) << 10) | (m >> 1);
+    /* Extra rounding. An overflow will set mantissa to 0 and increment
+                   * the exponent, which is OK. */
+    bits += m & 1;
+    return bits;
 }
