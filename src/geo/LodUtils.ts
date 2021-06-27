3f77e3f29d2651a967b9eebde023d93713241933
Peter Todd <ptr.tdd@gmail.com>
2021-06-27 17:15:50 +0100

    add some unused code for sub-meshes.

--- src/geo/LodUtils.ts
+++ src/geo/LodUtils.ts
@@ -33,10 +33,10 @@ function makeTileGeometry(s: number) {
   return geo;
 }
 
-export const LOD_LEVELS = 12;
+const LOD_LEVELS = 12;
 /** by LOD, 0 is 2k, 1 is 1k, 2 is 500... powers of 2 might've been nice if the original data was like that */
 ///// chchchanging....
-export const tileGeom: THREE.BufferGeometry[] = [];
+const tileGeom: THREE.BufferGeometry[] = [];
 for (let i=0; i<LOD_LEVELS; i++) {
     tileGeom.push(makeTileGeometry(Math.floor(4096 / Math.pow(2, i))));
 }
@@ -46,7 +46,7 @@ for (let i=0; i<LOD_LEVELS; i++) {
 //     const d = dist/tileRes;
 //     return Math.pow(2, Math.min(LOD_LEVELS-1, Math.round(Math.sqrt(d/lodFalloffFactor))));
 // }
-export function getLodUniforms(lod: number) {
+function getLodUniforms(lod: number) {
   const s = Math.pow(2, lod);//1, 2, 4, ...
   const w = 4096 / s; //4096, 2048, 1024, ...
   const e = 1/(w-1);
@@ -57,7 +57,7 @@ export function getLodUniforms(lod: number) {
   return {EPS, gridSizeX, gridSizeY, LOD};
 }
 
-export async function getTileMesh(info: DsmCatItem, lowRes = false) {
+export async function getTileMesh(info: DsmCatItem, lowRes = false, lodBias = 5) {
   //let's call this differently for a low-res layer.
   // let info = getTileProperties(coord, lowRes) || nullInfo;
   // if (info.mesh) return info; //!!! this breaks when more than one scene uses the same tile.
@@ -66,8 +66,8 @@ export async function getTileMesh(info: DsmCatItem, lowRes = false) {
   const sources = info.sources;
   const source = !sources ? info.source_filename : sources[2000] || sources[1000] || sources[500]!;
   const url = getImageFilename(source, lowRes);
-  
-  const {texture, frameInfo} = await JP2.jp2Texture(url, lowRes); //lowRes also means 'fullFloat' at the moment
+
+  const { texture, frameInfo } = await JP2.jp2Texture(url, lowRes); //lowRes also means 'fullFloat' at the moment
   const w = frameInfo.width, h = frameInfo.height;
   const lodObj = new THREE.LOD();
   const s = lowRes ? 40960 : 1000;
@@ -75,29 +75,65 @@ export async function getTileMesh(info: DsmCatItem, lowRes = false) {
   // and doesn't include stats. that might change, and we should more clearly flag.
   const eleScale = lowRes ? 1 : info.max_ele! - info.min_ele!;
   lodObj.scale.set(s, s, eleScale);
-  lodObj.position.z = info.min_ele??0;
-  for (let lod = 0; lod<LOD_LEVELS; lod++) {
-      const uvTransform = new THREE.Matrix3();
-      // uvTransform.scale(0.5, 0.5);
-      const uniforms = {
-          heightFeild: { value: texture },
-          heightMin: { value: info.min_ele?? 0 }, heightMax: { value: info.max_ele?? 1 },
-          ...getLodUniforms(lod),
-          uvTransform: { value: uvTransform },
-          iTime: globalUniforms.iTime,
-      };
-      const geo = tileGeom[lod]; //regardless of image geom, for now
+  lodObj.position.z = info.min_ele ?? 0;
+
+  for (let lod = 0; lod < LOD_LEVELS; lod++) {
+    if (false && lod <= 3) {
+      //considering QuadTree so that when zoomed-in we can cull some parts
+      //first pass, non-recursive but splitting up first few lod levels
+      //needs more work to be useful.
+      let g = new THREE.Group();
+      for (let y=0; y<2; y++) {
+        for (let x=0; x<2; x++) {
+          const uvTransform = new THREE.Matrix3();
+          // uvTransform.translate(x/2, y/2);
+          // uvTransform.scale(0.5, 0.5);
+          uvTransform.setUvTransform(0, 0, 0.5, 0.5, 0, x, y);
+          const uniforms = {
+            heightFeild: { value: texture },
+            heightMin: { value: info.min_ele ?? 0 }, heightMax: { value: info.max_ele ?? 1 },
+            ...getLodUniforms(lod+1),
+            uvTransform: { value: uvTransform },
+            iTime: globalUniforms.iTime,
+          };
+          //nb, using regular bbox will mean no culling of submeshes.
+          const geo = tileGeom[lod+1];
       
-      const mat = getTileMaterial(uniforms);
-      // mat.wireframe = true;
-      const mesh = new THREE.Mesh(geo, mat);
-      applyCustomDepth(mesh, uniforms);
-      mesh.castShadow = true;
-      mesh.receiveShadow = true;
-      
-      //this could be tweakable (integer only, higher values lower detail)
-      const lodBias = 8;
-      lodObj.addLevel(mesh, Math.pow(2,lod-lodBias) * s);
+          const mat = getTileMaterial(uniforms);
+          // mat.wireframe = true;
+          const mesh = new THREE.Mesh(geo, mat);
+          applyCustomDepth(mesh, uniforms);
+          // mesh.scale.set(1, 1, 1);
+          // mesh.position.set(x/2, y/2, 0);
+          // mesh.updateMatrix();
+          mesh.castShadow = true;
+          mesh.receiveShadow = true;
+          g.add(mesh);
+        }
+      }
+      lodObj.addLevel(g, Math.pow(2, lod - lodBias) * s);
+      continue;
+    }
+    const uvTransform = new THREE.Matrix3();
+    // uvTransform.scale(0.5, 0.5);
+    const uniforms = {
+      heightFeild: { value: texture },
+      heightMin: { value: info.min_ele ?? 0 }, heightMax: { value: info.max_ele ?? 1 },
+      ...getLodUniforms(lod),
+      uvTransform: { value: uvTransform },
+      iTime: globalUniforms.iTime,
+    };
+    const geo = tileGeom[lod]; //regardless of image geom, for now
+
+    const mat = getTileMaterial(uniforms);
+    // mat.wireframe = true;
+    const mesh = new THREE.Mesh(geo, mat);
+    applyCustomDepth(mesh, uniforms);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+
+    //lodBias is a +ve integer, higher values mean lower geometric detail
+    lodObj.addLevel(mesh, Math.pow(2, lod - lodBias) * s);
   }
 
   info.mesh = lodObj;
